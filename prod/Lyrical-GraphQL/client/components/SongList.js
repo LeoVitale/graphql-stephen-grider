613b7d1b988e514a85844308f582ef0c820e19ad
Leonardo Vitale <leonardo_vitale@hotmail.com>
2017-10-23 03:43:14 -0200

    38. Bonding Queries with Components

--- prod/Lyrical-GraphQL/client/components/SongList.js
+++ prod/Lyrical-GraphQL/client/components/SongList.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
   render() {
+    console.log('====================================');
+    console.log(this.props);
+    console.log('====================================');
     return (
       <div>
         SongList
@@ -19,4 +23,4 @@ const query = gql`
   }
 `;
 
-export default SongList;
+export default graphql(query)(SongList);
